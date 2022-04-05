a8fd6d428c7ee38e124502e707a9e48b647490e3
hiếu trần <hieudubstep2001@gmail.com>
2022-04-05 09:38:43 +0700

    Hieu dep trai add something

---
+++ nhomtien/HieuForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nhomtien
+{
+    public partial class HieuForm : Form
+    {
+        public HieuForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
